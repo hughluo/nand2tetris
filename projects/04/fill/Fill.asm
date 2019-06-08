e895b5d2e651ce2092dd1f93721f4765ae7fec9a
Yinchi, "Wexort", Luo <bugwriter@outlook.com>
2019-06-08 15:09:34 +0200

    project4 100pts

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,62 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+    // init addr point to the first chunk
+    @SCREEN
+    D=A
+    @addr
+    M=D
+    
+    // if key pressed goto BLACKOUT
+    @24576
+    D=M
+    @BLACKOUT
+    D;JNE
+
+    // else goto WHITEOUT
+
+(WHITEOUT)
+    // if loop through the screen, goto LOOP
+    @addr
+    D=M
+    @24576
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // set that 16bit chunck of screen to white
+    @addr
+    A=M
+    M=0
+    
+    // set addr pointer to the next chunck
+    @addr
+    M=M+1
+
+    @WHITEOUT
+    0;JMP
+
+
+(BLACKOUT)
+    // if loop through the screen, goto LOOP
+    @addr
+    D=M
+    @24576
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // set that 16bit chunck of screen to black
+    @addr
+    A=M
+    M=-1
+    
+    // set addr pointer to the next chunck
+    @addr
+    M=M+1
+
+    @BLACKOUT
+    0;JMP
